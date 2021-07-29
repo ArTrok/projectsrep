f425bf289401fc18bdcb509f6b0418e358a5dd7b
Arthur <arthurteixeira0805@gmail.com>
2021-07-29 14:46:29 -0300

    correcting

--- fundamentos/block-04-JavaScript-Introduction-and-logic-programming/multiplication.js
+++ fundamentos/block-04-JavaScript-Introduction-and-logic-programming/multiplication.js
@@ -1,4 +1,4 @@
-let factor1 = prompt('insert the first number for the sum');
-let factor2 = prompt('insert the second number for the sum');
+let factor1 = 5;
+let factor2 = 10;
 let result = factor1 * factor2
 console.log(result)
