471e87e3ef50d825afec7b17b37db8c13a3a883c
Arthur <arthurteixeira0805@gmail.com>
2021-07-29 19:42:03 -0300

    ex 8 9 10

---
+++ fundamentos/block-04-JavaScript-Introduction-and-logic-programming/check-for-even.js
@@ -0,0 +1,11 @@
+let number1 = 8
+let number2 = 8
+let number3 = 0
+let odd = true
+
+if (number1%2 !== 0 ||number2%2 !== 0 ||number3%2 !== 0) {
+    odd = true
+} else {
+    odd = false
+}
+console.log(odd)
